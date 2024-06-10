a36884b2ffc519c1c4679c4a8cf62f077853409a
pardosoft <rafapbs@hotmail.com>
2024-06-10 16:09:30 -0600

    Prueba de Registro

--- js/script.js
+++ js/script.js
@@ -1,43 +1,126 @@
 let ltCategorias = new Object();
 ltCategorias = getCategorias();
-getEstados();
-getCodigoPostal("66490");
+//getEstados();
+//getCodigoPostal("66490");
 
 const btnEnviar = document.getElementById("btn-enviar");
 btnEnviar.addEventListener('click', function () {
     /* Validar todos los campos */
-    /*let tEmail = document.getElementById("email");
-    let tTelefono = document.getElementById("telefono");
-    let tNombre = document.getElementById("nombre");
-    let tApellidoPaterno = document.getElementById("paterno");
-    let tApellidoMaterno = document.getElementById("materno");
-    let tCalle = document.getElementById("calle");
-    let tNumeroExt = document.getElementById("numeroExt");
-    let tColonia = document.getElementById("colonia");
-    let tCodigoPostal = document.getElementById("codigoPostal");
-    let sEstado = document.getElementById("selectEstado");
-    let sMunicipio = document.getElementById("selectMunicipio");
-    let tPais = document.getElementById("pais");
+    let tEmail = document.getElementById("email").value;
+    let tTelefono = document.getElementById("telefono").value;
+    let tNombre = document.getElementById("nombre").value;
+    let tApellidoPaterno = document.getElementById("paterno").value;
+    let tApellidoMaterno = document.getElementById("materno").value;
+    let tCalle = document.getElementById("calle").value;
+    let tNumeroExt = document.getElementById("numeroExt").value;
+    let tColonia = document.getElementById("colonia").value;
+    let tCodigoPostal = document.getElementById("codigoPostal").value;
+    let sEstado = document.getElementById("selectEstado").value;
+    let sMunicipio = document.getElementById("selectMunicipio").value;
+    let tPais = document.getElementById("pais").value;
+    let tDescripcion = document.getElementById("descripcion").value;
 
-    if(!validateEmail) alert("Escriba un correo válido.");
-    if(tTelefono.length == 0) alert("Escriba un número de teléfono.");
-    if(tNombre.length == 0) alert("Escriba su nomnbre.");
-    if(tApellidoPaterno.length == 0) alert("Escriba su apellido paterno.");
-    if(tApellidoMaterno.length == 0) alert("Escriba su apellido materno.");
-    if(tCalle.length == 0) alert("Escriba la calle donde vive.");
-    if(tNumeroExt.length == 0) alert("Escriba el numero exterior de su casa.");
-    if(tColonia.length == 0) alert("Escriba el nombre de su coloina.");
-    if(tCodigoPostal.length == 0) alert("Escriba su código postal.");
-    if(sEstado.value == null || sEstado.value < 1) alert("Elija el estado donde vive.");
-    if(sMunicipio.value == null || sMunicipio.value < 1) alert("Elija el municipio donde vive.");*/
+    if(tEmail.length == 0) {
+        alert("Escriba un correo electrónico válido.");
+        return;
+    }
+    if(!validateEmail(tEmail)) {
+        alert("Escriba un correo electrónico válido.");
+        return;
+    }
+    if(tTelefono.length == 0) {
+        alert("Escriba un número de teléfono.");
+        return;
+    }
+    if(tNombre.length == 0) {
+        alert("Escriba su nombre.");
+        return;
+    }
+    if(tApellidoPaterno.length == 0) {
+        alert("Escriba su apellido paterno.");
+        return;
+    }
+    if(tApellidoMaterno.length == 0) {
+        alert("Escriba su apellido materno.");
+        return;
+    }
+    if(tCalle.length == 0) {
+        alert("Escriba la calle donde vive.");
+        return;
+    }
+    if(tNumeroExt.length == 0) {
+        alert("Escriba el número exterior de su casa.");
+        return;
+    }
+    if(tColonia.length == 0) {
+        alert("Escriba el nombre de su colonia.");
+        return;
+    }
+    if(tCodigoPostal.length == 0) {
+        alert("Escriba su código postal.");
+        return;
+    }
+    /*if(sEstado.value == null || sEstado.value < 1) {
+        alert("Elija el estado donde vive.");
+        return;
+    }
+    if(sMunicipio.value == null || sMunicipio.value < 1) {
+        alert("Elija el municipio donde vive.");
+        return;
+    }*/
+    if(tDescripcion.length == 0) {
+        alert("Escriba una descripción de su trabjo.");
+        return;
+    }
 
     let checks = document.querySelectorAll(".checks-categorias:checked");
     let arrayChecks = new Array();
     for(let i = 0; i < checks.length; i++) {
-        arrayChecks.push(checks[i].value);
+        arrayChecks.push(checks[i].value, "X");
     }
 
     console.log(arrayChecks);
+
+    if(arrayChecks.length == 0) {
+        alert("Debe seleccionar al menos una categoría.");
+        return;
+    }
+
+    if(document.getElementById("fotos").files.length == 0 ){
+        alert("Debe seleccionar un archivo o fotografía de su trabajo.");
+        return;
+    }
+    if(document.getElementById("selfie").files.length == 0 ){
+        alert("Debe seleccionar un archivo o fotografía de su rostro.");
+        return;
+    }
+    if(document.getElementById("idOficial").files.length == 0 ){
+        alert("Debe seleccionar un archivo o fotografía de su identificación oficial.");
+        return;
+    }
+    if(document.getElementById("comprobanteDomicilio").files.length == 0 ){
+        alert("Debe seleccionar un archivo o fotografía de su comprobante de domicilio.");
+        return;
+    }
+
+    let registro = {
+        "email": tEmail,
+        "telefono": tTelefono,
+        "nombre": tNombre,
+        "apellidoPaterno": tApellidoPaterno,
+        "apellidoMaterno": tApellidoMaterno,
+        "calle": tCalle,
+        "numeroExt": tNumeroExt,
+        "colonia": tColonia,
+        "codigoPostal": tCodigoPostal,
+        "idEstado": 1,
+        "idMunicipio": 1,
+        "idPais": "1",
+        "descripcionTrabajo": tDescripcion,
+        "listCategorias": arrayChecks
+    }
+
+    postRegistro(registro);
 });
 
 const validateEmail = (email) => {
@@ -89,6 +172,18 @@ function getMunicipios(idEstado) {
             });
         })
         .catch(console.log("Error Municipios"))
+}
+
+async function postRegistro(pRegistro) {
+    const response = await fetch("https://micasaapptestapi.azurewebsites.net/api/registro", {
+        method: "POST",
+        body: JSON.stringify({registro: pRegistro}),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 }
 
 async function getCategorias(url = 'https://micasaapptestapi.azurewebsites.net/api/categorias', data = {}) {
